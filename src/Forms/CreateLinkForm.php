d2c27285c19549c08cf262255002bdae68e636a9
Dmitriy Derepko <xepozz@list.ru>
2020-01-08 15:04:30 +0300

    Создал форму

---
+++ src/Forms/CreateLinkForm.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Forms;
+
+use Phalcon\Forms\Element\Password;
+use Phalcon\Forms\Form;
+use Phalcon\Validation\Validator\Confirmation;
+use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Validation\Validator\StringLength;
+
+class CreateLinkForm extends Form
+{
+    public function initialize()
+    {
+        // Password
+        $password = new Password('password');
+        $password->addValidators(
+            [
+                new PresenceOf(
+                    [
+                        'message' => 'Password is required',
+                    ]
+                ),
+                new StringLength(
+                    [
+                        'min' => 8,
+                        'messageMinimum' => 'Password is too short. Minimum 8 characters',
+                    ]
+                ),
+                new Confirmation(
+                    [
+                        'message' => 'Password doesn\'t match confirmation',
+                        'with' => 'confirmPassword',
+                    ]
+                ),
+            ]
+        );
+        $this->add($password);// Confirm Password
+        $confirmPassword = new Password('confirmPassword');
+        $confirmPassword->addValidators(
+            [
+                new PresenceOf(
+                    [
+                        'message' => 'The confirmation password is required',
+                    ]
+                ),
+            ]
+        );
+        $this->add($confirmPassword);
+    }
+}
