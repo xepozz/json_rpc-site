d68b9db67b824ac3b2a0b79440b5052dea98c032
Dmitriy Derepko <xepozz@list.ru>
2020-01-09 20:31:00 +0300

    Сделал нормальную выдачу

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Actions\ShortenLinkAction;
+use App\Exceptions\BadRequestException;
 use App\Forms\CreateLinkForm;
 use App\Http\Rpc\Response;
 use Phalcon\Http\RequestInterface;
@@ -12,10 +13,11 @@ use Phalcon\Mvc\Controller;
 
 class IndexController extends Controller
 {
-    public function indexAction(): void
+    public function indexAction()
     {
         if ($this->request->isPost()) {
             $this->processRequest($this->request);
+            return $this->response->redirect();
         }
 
         $this->view->setLayout('create');
@@ -26,16 +28,34 @@ class IndexController extends Controller
 
     private function processRequest(RequestInterface $request): void
     {
-        $code = $request->get('link');
-        $this->flash->success('Your link: ' . $code);
+        $link = $request->get('link');
 
         /* @var $action \App\Actions\ShortenLinkAction */
         $action = $this->getDI()->get(ShortenLinkAction::class);
-        $result = $action->shorten($code);
+
+        try {
+            $result = $action->shorten($link);
+        } catch (BadRequestException $e) {
+            $this->flashSession->error($e->getMessage());
+
+            return;
+        }
+
         if ($result->hasErrors()) {
             $this->showErrors($result);
         } else {
-            $this->flash->success('Action result: ' . reset($result->getResult()));
+            $config = $this->di->get('config');
+            $url = $config['application']['publicUrl'];
+
+            $result = $result->getResult();
+            $code = reset($result);
+            $this->flashSession->success(
+                sprintf(
+                    'Your link: %s was successfully shortened to %s',
+                    $link,
+                    $url . '/' . $code
+                )
+            );
         }
     }
 
