8af935b75258472ce749b69ff8199e66b9eb1baa
Dmitriy Derepko <xepozz@list.ru>
2020-01-10 19:50:34 +0300

    Добавил валидацию на фронте

--- src/Forms/CreateLinkForm.php
+++ src/Forms/CreateLinkForm.php
@@ -6,22 +6,33 @@ namespace App\Forms;
 
 use Phalcon\Forms\Element\Text;
 use Phalcon\Forms\Form;
+use Phalcon\Validation\Validator\Callback;
+use Phalcon\Validation\Validator\PresenceOf;
 
 class CreateLinkForm extends Form
 {
     public function initialize()
     {
         $password = new Text('link');
-//        $password->addValidators(
-//            [
-//                new StringLength(
-//                    [
-//                        'min' => 8,
-//                        'messageMinimum' => 'Password is too short. Minimum 8 characters',
-//                    ]
-//                ),
-//            ]
-//        );
+        $password->addValidators(
+            [
+                new PresenceOf(
+                    [
+                        'message' => 'The :field is required',
+                    ]
+                ),
+                new Callback(
+                    [
+                        'callback' => function ($data) {
+                            $pattern = '#^https?://[\w\-.]+/?[.*]#';
+
+                            return preg_match($pattern, $data['link']) === 1;
+                        },
+                        'message' => ':field contain invalid url',
+                    ]
+                ),
+            ]
+        );
         $this->add($password);
     }
 }
